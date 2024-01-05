9b30aad12dedc3d3756562e7c579c76a94cbc223
kanth5 <mrajk5@gmail.com>
2024-01-05 15:39:21 +0530

    Prototype scoped bean life cycle

--- src/main/java/io/datajek/spring/basics/movierecommendersystem/lesson11/MovieRecommenderSystemApplication.java
+++ src/main/java/io/datajek/spring/basics/movierecommendersystem/lesson11/MovieRecommenderSystemApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
-
 @SpringBootApplication
 public class MovieRecommenderSystemApplication {
 
@@ -16,6 +15,7 @@ public class MovieRecommenderSystemApplication {
         ApplicationContext appContext = SpringApplication.run(MovieRecommenderSystemApplication.class, args);
 
         //use ApplicationContext to get recommender object
+        // Observe the sequence of events with constructor, @PostConstruct and @PreDestroy for RecommenderImplementation & its dependency ContentBasedFilter
         RecommenderImplementation recommender = appContext.getBean(RecommenderImplementation.class);
 
         System.out.println();
@@ -29,5 +29,15 @@ public class MovieRecommenderSystemApplication {
         System.out.println();
         System.out.println();
         System.out.println(Arrays.toString(result));
+
+        //Retrieving prototype bean from application context twice to show that @PreDestroy is not called by Spring and is the responsibility of the application
+        // to do the cleanup of the Prototype beans
+        System.out.println();
+        Movie m1 = appContext.getBean(Movie.class);
+        System.out.println(m1);
+
+        System.out.println();
+        Movie m2 = appContext.getBean(Movie.class);
+        System.out.println(m2);
     }
 }

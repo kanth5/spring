572959342c3cd589b0d87868f2fd662a1cf0f7c7
kanth5 <mrajk5@gmail.com>
2024-01-05 19:10:01 +0530

    Configuring a Basic Spring Application

---
+++ src/main/java/io/datajek/spring/basics/movierecommendersystem/lesson13/RecommenderImplementation.java
@@ -0,0 +1,24 @@
+package io.datajek.spring.basics.movierecommendersystem.lesson13;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RecommenderImplementation {
+
+    @Autowired
+    //use filter interface to select filter. Autowiring by Type is the default if no information is available.
+    private Filter filter;
+
+    //use a filter to find recommendations
+    public String [] recommendMovies (String movie) {
+
+        //print the name of interface implementation being used
+        System.out.println("Name of the filter in use: " + filter + "\n");
+
+        String[] results = filter.getRecommendations("Finding Dory");
+
+        return results;
+    }
+
+}

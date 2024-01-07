65624987851eeb22057b5c602212e9f68806d715
kanth5 <mrajk5@gmail.com>
2024-01-07 15:48:35 +0530

    XML configuration in Spring

---
+++ src/main/java/io/datajek/spring/basics/movierecommendersystem/lesson14/ContentBasedFilter.java
@@ -0,0 +1,13 @@
+package io.datajek.spring.basics.movierecommendersystem.lesson14;
+
+public class ContentBasedFilter implements Filter {
+
+    public String[] getRecommendations(String movie) {
+
+        //implement logic of content based filter
+
+        //return movie recommendations
+        return new String[]{"Happy Feet", "Ice Age", "Shark Tale"};
+    }
+
+}
